9f17a5d41725a7888c87239d71d001686f05406a
aamir <16462830+batakpout@users.noreply.github.com>
2024-05-25 14:08:35 +0530

    Update cin_get.cpp

--- strings_chararrays/cin_get.cpp
+++ strings_chararrays/cin_get.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 using namespace std;
+     //IT READS ONE CHAR AT A TIME FROM INPUT BUFFER
+
 //when cin.get is manipulated through loops we can get it to enter mutli lines and a delimiter at the end
 int main() {
     /**
      * As soon as we enter new line e.g hello in console the console sends the chars
        to the input buffer and cin.get picks chars from there one by one
     */
-     
     char temp = cin.get(); //if we enter hello in console it will just read h and then in while loop next cin.get will get e and so and once done with hello waits for the next char from the user to enter 
     cout << endl<<"First char entered:" << temp << endl;
     //while(temp !='\n')
